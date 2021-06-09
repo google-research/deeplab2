1f40f73d7d62edae9bdeff2c393315d66df4e505
yukun <yukun@google.com>
2021-06-08 17:28:13 -0700

    Adding export test for axial resnet models.
    PiperOrigin-RevId: 378285384

--- model/encoder/axial_resnet_instances_test.py
+++ model/encoder/axial_resnet_instances_test.py
@@ -17,12 +17,15 @@
 
 import os
 
+from absl import flags
 from absl.testing import parameterized
 import numpy as np
 import tensorflow as tf
 
 from deeplab2.model import test_utils
 from deeplab2.model.encoder import axial_resnet_instances
+
+FLAGS = flags.FLAGS
 
 
 class AxialResnetInstancesTest(tf.test.TestCase, parameterized.TestCase):
@@ -199,6 +202,33 @@ class AxialResnetInstancesTest(tf.test.TestCase, parameterized.TestCase):
     self.assertAllClose(output / normalizing_factor,
                         expected / normalizing_factor,
                         atol=1e-4, rtol=1e-4)
+
+  @parameterized.parameters(
+      ('resnet50',),
+      ('resnet50_beta',),
+      ('max_deeplab_s_backbone',),
+      ('max_deeplab_l_backbone',),
+      ('axial_resnet_s',),
+      ('axial_resnet_l',),
+      ('axial_deeplab_s',),
+      ('axial_deeplab_l',),
+      ('swidernet',),
+      ('axial_swidernet',),
+      )
+  def test_model_export(self, model_name):
+    model = axial_resnet_instances.get_model(
+        model_name,
+        output_stride=16,
+        backbone_layer_multiplier=1.0,
+        bn_layer=tf.keras.layers.BatchNormalization,
+        conv_kernel_weight_decay=0.0001,
+        # Disable drop path as it is not compatible with model exporting.
+        block_group_config={'drop_path_keep_prob': 1.0})
+    model(tf.keras.Input([257, 257, 3], batch_size=1), training=False)
+    export_dir = os.path.join(
+        FLAGS.test_tmpdir, 'test_model_export', model_name)
+    model.save(export_dir)
+
 
 if __name__ == '__main__':
   tf.test.main()
